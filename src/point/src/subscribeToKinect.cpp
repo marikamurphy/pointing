b87aab8155daaf5c3f27ed483416f693c2b2db88
ljernigan447 <ljernigan@gmail.com>
2020-07-09 20:22:58 -0500

    Refactoring subscribeToKinect code.

--- src/point/src/subscribeToKinect.cpp
+++ src/point/src/subscribeToKinect.cpp
@@ -2,6 +2,7 @@
 #include "point/TFBroadcastPR.h"
 #include "tf2_ros/message_filter.h"//do we need?
 #include <cv_bridge/cv_bridge.h>
+#include <Eigen/Dense>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <math.h>
 #include <message_filters/subscriber.h>
@@ -25,24 +26,22 @@
 class SubscribeToKinect {
 private:
 
-    //struct to store camera parameters
+    /* Struct to store camera parameters */
+    Matrix3f camCal;
     struct {
         float fx = 0;
         float fy = 0;
         float cx = 0; 
         float cy = 0;
     } camCal;
-
-    TFBroadcastPR broadPRNavFix; 
     cv::Mat depth_image;
     cv::Mat color_image;
-    //sensor_msgs::Image image;
     op::Wrapper& opWrapper;
 
 public:
     ros::Publisher marker_pub;
-    /* Constructor for Subscribe to Kinect.  Initializes opWrapper and broadPRNavFix. */
-    SubscribeToKinect(op::Wrapper& wrapper) : opWrapper(wrapper), broadPRNavFix(ROOT_TRANSFORM, "offset_navfixed") { }
+    /* Constructor for Subscribe to Kinect.  Initializes opWrapper. */
+    SubscribeToKinect(op::Wrapper& wrapper) : opWrapper(wrapper)) { }
 
     void masterCallback(const sensor_msgs::Image::ConstPtr &color, const sensor_msgs::Image::ConstPtr &depth) {
         ROS_INFO("OMG LOL");
@@ -50,45 +49,38 @@ public:
         depth_image = cv_bridge::toCvCopy(depth, depth->encoding)->image;
         color_image = cv_bridge::toCvCopy(color, color->encoding)->image;
 
-        //cv::imshow("1", depth_image);
-        //cv::imshow("2", color_image);
-        //cv::waitKey(1);
-        //now we run openpose on it
-        getKeyPoints(color_image);
+        cv::imshow("1", depth_image);
+        cv::imshow("2", color_image);
+        cv::waitKey(1);
+        /* Now we run openpose on it. */
+        getKeyPoints();
     } 
 
-    //adjust a x, y, depth point to reflect the camera calibration
-    geometry_msgs::Point transformPoint(int x, int y, float depth_val){
-        geometry_msgs::Point point;
-        point.x = (x + 0.5 - camCal.cx) * camCal.fx * depth_val;
-        point.y = (y + 0.5 - camCal.cy) * camCal.fy * depth_val;
-        point.z = depth_val;
-
-        return point;
-    }
-    
-    //get the camera calibration depth
+    /* Get camera calibration values.  Adjust them and put into camera calibration matrix. */
     void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg) {
-        camCal.fx = 1/msg->K[0]; // we need to adjust
-        camCal.fy = 1/msg->K[4];
-        camCal.cx = msg->K[2];
-        camCal.cy = msg->K[5];
-    }
+        // TODO Do we need to do the 1/msg thing?
+        float fx = 1/msg->K[0];
+        float fy = 1/msg->K[4];
+        float cx = msg->K[2];
+        float cy = msg->K[5];
+        camCal << fx,  0, cx,
+                   0, fy,  0,
+                   0,  0,  1;
+        return camCal;
+    }    
 
     // Adapted from https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/examples/tutorial_api_cpp/01_body_from_image_default.cpp
-    // For getting key points from an image
-    int getKeyPoints(const cv::Mat cvImageToProcess) {
-        // opLog is like print?
-        // op::opLog("Printing key points: ", op::Priority::High);
-        // Instead of doing this, we need to pass in  the cv Mats we get in the subscriber
-        //const cv::Mat cvImageToProcess = cv::imread(FLAGS_image_path);
-        const op::Matrix imageToProcess = OP_CV2OPCONSTMAT(cvImageToProcess);
-        auto datumProcessed = opWrapper.emplaceAndPop(imageToProcess);
-        // Error checking datum processed
-        if (datumProcessed != nullptr) {
-            printKeypoints(datumProcessed);
+    /* Get the DatumProcessed with all of the information on body key points. */
+    auto getKeyPoints() {
+        const op::Matrix imageToProcess = OP_CV2OPCONSTMAT(color_image);
+        auto datum = opWrapper.emplaceAndPop(imageToProcess);
+        /* Error checking datum processed. */
+        if (datum != nullptr) {
+            //printKeypoints(datum);
+            return datum;
         } else {
             op::opLog("Image could not be processed.", op::Priority::High);
+            return Null;
         }
     }
 
@@ -199,8 +191,9 @@ public:
         return ret;
     }
     
-    // Show the two points on the point vector
+    /* Show points on vector in rviz. Creates a new geometry_msg::Point each time it is called. */
     void printPoints (std::vector<geometry_msgs::Point> pts) {
+        /* Create visualization msg and set parameters. */
         visualization_msgs::Marker points;
         points.header.frame_id = ROOT_TRANSFORM;
         points.header.stamp = ros::Time::now();
@@ -208,182 +201,43 @@ public:
         points.action = visualization_msgs::Marker::ADD;
         points.pose.orientation.w = 1.0;     
         points.color.a = 1.0;
-
-        points.points.push_back(pts[0]);
-        points.points.push_back(pts[1]);
-
-
         points.color.g = 1.0;
-
         points.scale.x = 0.2;
         points.scale.y = 0.2;
-
         points.id = 0;
-
         points.type = visualization_msgs::Marker::POINTS;
+        /* Put points onto msg. */
+        for (geometry_msgs::Point pt : pts) {
+            points.points.push_back(pt);
+        }
+        /* Publish points to show on screen. */
         marker_pub.publish(points);
     }
 
-    /* Determine which pixels are being crossed by the section of the line being viewed. */  
-    void bresLine(std::vector<geometry_msgs::Point> line) {
-      std::vector<geometry_msgs::Point> grid_line;
-      grid_line = bresenham(line);
-      int i;
-      int search_area = 20; //num pixels above/ below line to search
-      for(int i = 0; i < grid_line.size(); i++){
-          float x = grid_line[i].x;
-          float y = grid_line[i].y;
-          //check above the line
-          if(withinImage(x, y+search_area)) 
-            objectIntersection(x, y+search_area);
-          //check below the line
-          if(withinImage(x, y-search_area))
-            objectIntersection(x, y-search_area);
+    /* Initialize camera intrisic parameters. */
+    Eigen::Matrix3f setCamCal (float fx, float fy, float cx, float cy) {
+       camCal << fx,  0, cx,
+                   0, fy,  0,
+                   0,  0,  1;
+        return camCal;
     }
 
-    }
-    //check if x,y is valid pixel within coordinates of the image
-    bool withinImage(float x, float y){
-       if(x > -1 && x < color_image.rows && y > -1 && y < color_image.cols)
-            return true;
-        return false; 
-    }
-    //check if given pixel lies within bounding box of an object
-    //TODO: return which object
-    void objectIntersection(float x, float y){
-
-    }
-
-    /* Get the coordinates of the pixels that need to be searched
-     * Input: Two points which are the pixels with the depth value at that spot.
-     * Output: A vector of pixels which are crossed by the line
-     */
-    std::vector<geometry_msgs::Point> bresenham (std::vector<geometry_msgs::Point> line) {
-        // Make sure we have 2 valid points
-        std::vector<geometry_msgs::Point> grid_line;
-        if (line.size() < 2 )  {
-            return grid_line;
-        }
-        
-        float x1 = line[0].x;
-        float y1 = line[0].y;
-        float x2 = line[1].x;
-        float y2 = line[1].y;
-
-        float m_new = 2 * (y2 - y1); 
-        float slope_error_new = m_new - (x2 - x1); 
-        for (float x = x1, y = y1; x <= x2 && x >= 0 && x < SCREEN_WIDTH; x++) 
-        { 
-            geometry_msgs::Point cor;
-            cor.x = x;
-            cor.y = y;
-            grid_line.push_back(cor);
-            // cout << "(" << x << "," << y << ")\n"; 
-        
-            // Add slope to increment angle formed 
-            slope_error_new += m_new; 
-        
-            // Slope error reached limit, time to 
-            // increment y and update slope error. 
-            if (slope_error_new >= 0) 
-            { 
-                y++; 
-                slope_error_new  -= 2 * (x2 - x1); 
-            } 
-        } 
-        return grid_line;
-    }
-
-    /*
-     * Magnitude of a vector
-     */
-    float magnitude(const float *vec, int n) {
-        float total = 0.0;
-        for (int i = 0; i < n; i++)
-    	total += vec[i] * vec[i];
-        return sqrt(total);
-    }
-    
-    /*
-     * Stores the cross product of v1 and v2 in output
-     * Strictly must be defined for vectors of size 3, so we don't need a size
-     * 
-     * Definition of cross product is overly complicated, but this is used
-     * in the calculation later for distance from a point to a line
-     *
-     * This can be understood as a simplification of the formula:
-     * | i	   j     k     |
-     * | v1[0] v1[1] v1[2] |
-     * | v2[0] v2[1] v2[2] |
-     * where you are taking the determinant of that matrix and
-     * i,j,k are the orthonormal element vectors
-     */
-    void crossProduct(const float v1[3], const float v2[3], float output[3]) {
-    	output[0] = v1[1] * v2[2] - v1[2] * v2[1];
-    	output[1] = v1[2] * v2[0] - v1[0] * v2[2];
-    	output[3] = v1[0] * v2[1] - v1[1] * v2[0];
-    }
-    
-    
-    /*
-     * As per http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
-     * This solves for the minimum distance from pt (an <x,y,z> vector) to the
-     * vector formed between points v1 and v2
-     *
-     * We can use this to test the distance from each point along our 2d line
-     * to the gesture created by the point
-     */    
-    float pointLineDistance3d(const float v1[3], const float v2[3], const float pt[3]) {
-    	float den[3];    // Denominator vector: v2 - v1
-    	float d1[3];     // Numerator left half: pt - v1
-    	float d2[3];     // Numerator right half: pt - v2
-    	float cross[3];  // Numerator vector: d1 x d2
-    	// Set up vectors
-    	for (int i = 0; i < 3; i++) {
-    	    den[i] = v2[i] - v1[i];
-    	    d1[i] = pt[i] - v1[i];
-    	    d2[i] = pt[i] - v2[i];
-    	}
-    	crossProduct(d1, d2, cross);
-
-    	// Return calculation
-    	return magnitude(cross, 3) / magnitude(den, 3);
-    }
-	
 };
 
 
 int main(int argc, char **argv) {
  ros::init(argc, argv, "point_node");
     ros::NodeHandle node;
-    cv::Mat img = cv::imread("/home/users/mmurphy/pointing/src/donut.png");
-
-    Client client(img);
-    client.connection();
-    /* op::Wrapper opWrapper{op::ThreadManagerMode::Asynchronous};
-
+    /* Set up opWrapper and point to the right model folder. */
+    op::Wrapper opWrapper{op::ThreadManagerMode::Asynchronous};
     try {
-        // Use single thread for debugging
-        //if (FLAGS_disable_multi_thread) {
-        //    opWrapper.disableMultiThreading();
-        //} //only use if openpose build with flags enabled
-        
-
         op::WrapperStructPose wrapperStructPose{};
         wrapperStructPose.modelFolder = op::String("openpose/models");
-        
         opWrapper.configure(wrapperStructPose);
         opWrapper.start();
-        op::PoseModel& poseModel = wrapperStructPose.poseModel;
-        const auto& pbpmBody25 = getPoseBodyPartMapping(poseModel);
-
-        // std::cout << "get 3 (relbow?) " << pbpmBody25.at(3) << std::endl; 
-        // std::cout << "get 4 (rwrist?) " << pbpmBody25.at(4) << std::endl;
-        // std::cout << "pose model: " << static_cast<int>(poseModel) << std::endl;
     } catch (const std::exception& e) {
         return -1;
     }
-
 
     SubscribeToKinect kSub(opWrapper);
 
@@ -401,7 +255,7 @@ int main(int argc, char **argv) {
     //ros::Subscriber ptCloud = node.subscribe("camera/depth/points",
      //   10, &SubscribeToKinect::printPtCloud, &kSub);
     kSub.marker_pub = node.advertise<visualization_msgs::Marker>("visualization_marker", 10);
-    */
+    
     std::cout<<"running"<<std::endl;
     ros::spin();
     std::cout<<"running"<<std::endl;
