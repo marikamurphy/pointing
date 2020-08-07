3527ac98d2cfe6f6dcfb03bb38590c5b529fd73b
ljernigan447 <ljernigan447@gmail.com>
2020-08-06 20:28:44 -0500

    Extended line and found intersections with the boundaries.  We still need to figure out which edge it hits.

--- src/point_interpretation/point_interpretation.cpp
+++ src/point_interpretation/point_interpretation.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc.hpp>
 #include <Eigen/Dense>
 #include <iostream>
+#include <unistd.h>
  
 using namespace cv;
 using namespace Eigen;
@@ -46,14 +47,14 @@ MatrixXd computeCartesianFromHomogeneous(MatrixXd in_mat) {
     return cart_point;
 }
 
-void renderCrosshair(int x, int y, cv::Mat &in_mat) {
-    line(in_mat, Point(x - 5,y), Point(x+5,y), Scalar(0,255,0));
-    line(in_mat, Point(x,y-5), Point(x,y+5), Scalar(0,255,0));
+void renderCrosshair(int x, int y, cv::Mat &in_mat, int rgb[]) {
+    line(in_mat, Point(x - 5,y), Point(x+5,y), Scalar(rgb[0],rgb[1],rgb[2]));
+    line(in_mat, Point(x,y-5), Point(x,y+5), Scalar(rgb[0],rgb[1],rgb[2]));
 }
 
-void renderCrosshair(MatrixXd in_pts, cv::Mat &in_mat) {
+void renderCrosshair(MatrixXd in_pts, cv::Mat &in_mat, int rgb[]) {
     for(int i = 0; i < in_pts.cols(); i++)
-        renderCrosshair(in_pts(0,i), in_pts(1,i), in_mat);
+        renderCrosshair(in_pts(0,i), in_pts(1,i), in_mat, rgb);
 }
 
 MatrixXd simpleCube(double half_width) {
@@ -111,31 +112,104 @@ MatrixXd simpleCube(double half_width) {
     return point_hom;
 }
 
+MatrixXd makeArm() {
+    MatrixXd arm(4, 2);
+    // Elbow
+    arm(0, 0) = 2;
+    arm(1, 0) = 2;
+    arm(2, 0) = 7;
+    arm(3, 0) = 1;
+    // Wrist
+    arm(0, 1) = 1;
+    arm(1, 1) = 1;
+    arm(2, 1) = 5;
+    arm(3, 1) = 1;
+    return arm;
+}
+
+MatrixXd extendArm(MatrixXd arm, int scale){
+    MatrixXd extendedArm(4, 2);
+    extendedArm.col(0) = arm.col(0);
+    extendedArm(0, 1) = arm(0,1) + scale * (arm(0,1) - arm(0,0)); // scale each point
+    extendedArm(1, 1) = arm(1,1) + scale * (arm(1,1) - arm(1,0));
+    extendedArm(2, 1) = arm(2,1) + scale * (arm(2,1) - arm(2,0));
+    extendedArm(3, 1) = 1;
+    return extendedArm;
+}
+
+Vector2d intersectionOfTwoLines(Vector2d A, Vector2d B, Vector2d C, Vector2d D) { 
+    Vector2d intersection();
+    // Line AB represented as a1x + b1y = c1 
+    double a1 = B(1) - A(1); 
+    double b1 = A(0) - B(0); 
+    double c1 = a1*(A(0)) + b1*(A(1)); 
+    
+    // Line CD represented as a2x + b2y = c2 
+    double a2 = D(1) - C(1); 
+    double b2 = C(0) - D(0); 
+    double c2 = a2*(C(0))+ b2*(C(1)); 
+    
+    double determinant = a1*b2 - a2*b1; 
+    
+    if (determinant == 0) 
+    { 
+        // The lines are parallel. This is simplified 
+        // by returning a pair of FLT_MAX 
+        intersection(0) = Double.MAX_VALUE;
+        intersection(1) = Double.MAX_VALUE;
+        return intersection; 
+    } 
+    else
+    { 
+        intersection(0) = (b2*c1 - b1*c2)/determinant; 
+        intersection(1) = (a1*c2 - a2*c1)/determinant; 
+        return intersection; 
+    } 
+} 
+
 int main(int argc, char **argv) {
-    MatrixXd point_hom = simpleCube(5);
+    //MatrixXd point_hom = simpleCube(5);
+    MatrixXd arm = makeArm();
+    MatrixXd extendedArm = extendArm(arm, 1);
 
     MatrixXd point_trans(4,1);
     point_trans(0,0) = 0;
     point_trans(1,0) = 0;
-    point_trans(2,0) = 50;
+    point_trans(2,0) = 0;
     point_trans(3,0) = 1;
 
     namedWindow( "out_image", WINDOW_AUTOSIZE);
-
-    for(double z = 0; z < 1000.0; z+= 0.01) {
-        point_trans(0,0) = z;
+    int key;
+    for(double z = 0; z < 10; z+= 0.001) {
+        extendedArm = extendArm(arm, z);
         
         MatrixXd camera_intrinsic = computeCameraIntrinsicMatrix(304, 305, 320, 240);
         MatrixXd camera_projective_matrix = camera_intrinsic * computeIdealProjection(point_trans);
 
-        MatrixXd new_point_hom = camera_projective_matrix * point_hom;
+        MatrixXd new_arm = camera_projective_matrix * arm;
+        MatrixXd new_extended_arm = camera_projective_matrix * extendedArm;
 
         Mat out_image = Mat::zeros(480, 640, CV_8UC3);
-        renderCrosshair(computeCartesianFromHomogeneous(new_point_hom), out_image);
+        int green_rgb[3] = {0, 255, 0};
+        MatrixXd arm_in_2d = computeCartesianFromHomogeneous(new_arm);
+        double m = (arm_in_2d(0, 0) - arm_in_2d(0, 1)) / (arm_in_2d(1, 0) - arm_in_2d(1, 1));
+        
+        double x_left = -arm_in_2d(1, 1) / m + arm_in_2d(0, 1); // 
+        double y_top = -m * arm_in_2d(0, 1) + arm_in_2d(1, 1);
+        double x_right = (480 - arm_in_2d(1, 1)) / m + arm_in_2d(0, 1);
+        double y_bottom = m * (640 - arm_in_2d(0, 1)) + arm_in_2d(1, 1);
+        // y1 - y = dy/dx(x1 - x)
+        renderCrosshair(arm_in_2d, out_image, green_rgb);
+        int red_rgb[3] = {255, 0, 0};
+        renderCrosshair(computeCartesianFromHomogeneous(new_extended_arm), out_image, red_rgb);
 
         imshow("out_image", out_image);
-
-        waitKey(1);
+        //usleep(50);
+        waitKey(2);
+        // key = waitKey(100);
+        // if(key == 27){
+        //     break;
+        // }
     }
 
     return 0;
